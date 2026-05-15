80e06531e93dce8ca8411f869cdfd840f7b460bf
s1140412-commits <s1140412@dmhs.kh.edu.tw>
2026-05-15 11:25:50 +0800

    Create variable.cpp

---
+++ variable.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int age;
+    string name;
+    cout << "請輸入年齡" << end; 
+    cin >> age;
+    cout << "請輸入名字" << end; 
+    cin >> name;
+    cout << "你是" << name << "今年" << age << "歲" << end1; 
+}
