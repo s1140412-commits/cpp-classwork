618058af91c66db9c27c029c9ab8553fe44091e4
s1140412-commits <s1140412@dmhs.kh.edu.tw>
2026-06-12 11:00:14 +0800

    Create array.cpp

---
+++ array.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double catweight[6] = {11.2, 10.8, 10.4, 11.5, 12.0, 13.0};
+    double sum = 0;
+    for  (int i=0,i<=5; i++){
+    	 sum = sum+ catweight[i];
+	}
+count<<"貓咪的平均體:" <<sum/6; 
+     return 0;
+}
